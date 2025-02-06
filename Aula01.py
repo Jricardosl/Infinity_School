ea76ff7cb61795368e7510df4f0b458174883652
Ricardo Lima <jricardosl.jrdsl@gmail.com>
2025-02-05 21:11:10 -0300

    Primeiro commit

---
+++ Aula01.py
@@ -0,0 +1,32 @@
+# print("Olá, mundo!")
+
+#Atividade 1
+# Criando a variável chamada 'saudacao'
+#saudacao = "Hello World"
+
+# Imprimindo o valor da variável
+#print(saudacao)
+
+# Verificando o tipo da variável
+#print(type(saudacao))
+
+#Atividade 2
+
+# Crie um programa que peça ao usuário para digitar:
+#1.Seu nome;
+#2.Sua idade;
+#3.Sua altura;
+#4.Em seguida, imprima esses valores e seus respectivos tipos.
+
+#Objetivo: Praticar a declaração e uso de variáveis de diferentes tipos,
+#além de aprender a verificar e imprimir seus tipos em Python.
+#Observação: Conhecer e utilizar diferentes tipos de variáveis
+#é fundamental para manipular dados de maneira eficaz.
+
+nome = input("Meu nome é: ")
+idade = input("Tenho a idade de: ")
+altura = input("e minha altura é de: ")
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
