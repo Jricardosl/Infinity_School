8e816f1708e270973067d9ed028d597996a89fce
Ricardo <jricardosl.jrdsl@gmail.com>
2025-07-10 15:45:31 -0300

    Final da atualização do arquivo Aula04.py

--- Aula04.py
+++ Aula04.py
@@ -207,6 +207,7 @@
 # else:
 #     print("Classificação: Reprovado")
 
+
 #FOR + IF + BREAK
 #Exemplo
 # for numero in range (10):
@@ -225,3 +226,122 @@
 #         print(f"{numero} é impar, continuando.") 
 # else:
 #     print ("Todos os números foram impares")
+
+#EXEMPLO MENU INTERATIVO
+# while True:
+#     print("Menu")
+#     print("1. Contar de 1 a 5")
+#     print("2. Sair")
+#     opcao = input("Escolha uma opção: ")
+    
+#     if opcao == "1":
+#         for i in range (1,6):
+#             print(i)
+#     elif opcao == "2":
+#         print ("Saindo do programa.")
+#         break
+#     else:
+#         print("Opção invalida, tente novamente")
+
+# #FOR + Condicional + BREAK e Continue
+
+# #Exemplo: Encontrar o primeiro número divisível por 3 e pular números divisíveis por 2.
+# for numero in range (1,20):
+#     if numero % 2 == 0:
+#         continue #pular números divisiveis por 2
+#     if numero % 3 == 0:
+#         print (f"Número {numero} é o primeiro divisível por 3 (não é divisível por 2).")
+#         break
+#     print(f"Número não divisível por 2: {numero}")
+
+#ANINHAMENTO DE FOR 
+#Exemplo: Filtrar Pares e Ímpares em um Intervalo
+
+# for i in range (1,6):
+#     for j in range (1,6):
+#         if (i + j) % 2 ==0:
+#             print(f"{i} + {j} é par")
+#         else:
+#             print(f"{i} + {j} é ímpar")
+
+# Atividade 09:
+# Verificar Números Pares e Impares com Interrupção:
+# Crie um programa que use um laço for para contar de 1 a 20. Use condicionais para
+# identificar números pares e ímpares. Pare o loop ao encontrar o número 15, usando break.
+
+# for i in range(1, 21):
+#     if i == 15:
+#         print(f"{i} é ímpar, processo interrompido.")
+#         break
+#     elif i % 2 == 0:
+#         print(f"{i} é par")
+#     else:
+#         print(f"{i} é ímpar")
+        
+
+# Atividade 10:
+# Contar Números Positivos e Negativos:
+# Peça ao usuário para inserir 10 números. Use um laço for com condicionais 
+# para contar quantos são positivos e quantos são negativos.
+# Pare o loop se o número 0 for inserido, usando break.
+
+# positivos = 0
+# negativos = 0
+
+# for i in range(10):
+#     numero = int(input(f"Digite o {i+1}º número: "))
+#     if numero ==0:
+#         print(f"O número digitado é zero. Encerrando a contagem.")
+#         break 
+#     elif numero > 0:
+#         positivos += 1
+#     else:
+#         negativos += 1
+
+#     # Se for zero, não conta como positivo nem negativo
+
+# print(f"\nQuantidade de números positivos: {positivos}")
+
+# print(f"Quantidade de números negativos: {negativos}")
+
+
+
+# Atividade 11:
+# Verificar Múltiplos de 5 e Parar:
+# Escreva um programa que use um laço for para imprimir números de 1 a 30.
+# Use uma condicional para verificar se um número é múltiplo de 5 e outro
+# para verificar se é maior que 20 e interromper o loop com break.
+
+# for i in range(1, 31):  # Vai de 1 até 30 (inclusive)
+#     if i > 20:
+#         print(f"{i} é maior que 20. Encerrando o loop.")
+#         break
+#     if i % 5 == 0:
+#         print(f"{i} é múltiplo de 5")
+#     else:
+#         print(i)
+
+
+# Atividade 12:
+# Soma de Números com Desconto:
+# Peça ao usuário para inserir 5 preços de produtos. 
+# Use um laço for para calcular o total. Aplique um desconto de 10% 
+# se o total ultrapassar 100 e interrompa o loop com break.
+
+# soma = 0
+
+# for i in range(5):
+#     preco = float(input(f"Digite o {i+1}º preço do produto: "))
+#     soma += preco
+
+#     if soma > 100:
+#         desconto = soma * 0.10
+#         total_com_desconto = soma - desconto
+#         print(f"O total R$ {soma:.2f} ultrapassou R$ 100,00.")
+#         print(f"Desconto de 10% aplicado: R$ {desconto:.2f}")
+#         print(f"Total com desconto: R$ {total_com_desconto:.2f}")
+#         print("Processo interrompido.")
+#         break
+# else:
+#     print(f"\nTotal dos produtos: R$ {soma:.2f}")
+#     print("Sem desconto aplicado.")
